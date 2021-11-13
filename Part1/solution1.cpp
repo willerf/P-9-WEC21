d9183cf02ac07758e1184380090b68fbcd00161b
rosscleary <58639828+rosscleary@users.noreply.github.com>
2021-11-13 14:49:14 -0500

    Add comments to part 1 and 2 code

--- Part1/solution1.cpp
+++ Part1/solution1.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 
 int wordTime(string word, map<int, string> buttonCharacters, map<char, int> characterButtons) {
+    // Creates a string that is the lowercase version of the word string
     string lowercaseWord = "";
     for(int i = 0; i < word.size(); i++) {
         if(word[i] - 'A' < 26) {
@@ -16,24 +17,30 @@ int wordTime(string word, map<int, string> buttonCharacters, map<char, int> char
         }
     }
 
+    // Calculates the time to type the word represented as number of quarter seconds
     int time = 0;
     for(int i = 0; i < word.size(); i++) {
+        // If the word is a capital
         if(word[i] - 'A' < 26) {
             time += 8;
         } 
         
+        // Check if the current button is same as last
         if(i != 0 && characterButtons[lowercaseWord[i]] == characterButtons[lowercaseWord[i - 1]]) {
             time++;
         }
 
+        // Find the number of clicks on the button needed
         int letterPosition = buttonCharacters[characterButtons[lowercaseWord[i]]].find(lowercaseWord[i]);
         time += letterPosition + 1;
     }
 
+    // Return the time minus 1 since the last character does not require a quarter second pause
     return time - 1;
 }
 
 int main() {
+    // Map to map buttons to their ordered characters
     map<int, string> buttonCharacters = {
         {2, "abc"},
         {3, "def"},
@@ -45,6 +52,7 @@ int main() {
         {9, "wxyz"}
     };
 
+    // Map to map characters to their button
     map<char, int> characterButtons = {
         {'a', 2}, {'b', 2}, {'c', 2},
         {'d', 3}, {'e', 3}, {'f', 3},
@@ -56,8 +64,10 @@ int main() {
         {'w', 9}, {'x', 9}, {'y', 9}, {'z', 9}
     };
 
+    // List of the words in the input
     vector<string> words;
 
+    // Open the file
     ifstream indata;
     string input;
     indata.open("Test1.txt");
@@ -66,6 +76,7 @@ int main() {
         exit(1);
     }
 
+    // Take in the input and add them to the list
     indata >> input;
     words.push_back(input);
     while (!indata.eof()) {
@@ -74,16 +85,20 @@ int main() {
     }
     indata.close();
 
+    // Add the times for each word to a list
     vector<int> times;
+    // Stores the minimum of the times
     int minimumTime = 1000000;
     for(int i = 0; i < words.size(); i++) {
         int currentTime = wordTime(words[i], buttonCharacters, characterButtons);
         times.push_back(currentTime);
+        // Updates the minimum time
         if(currentTime < minimumTime) {
             minimumTime = currentTime;
         }
     }
 
+    // Outputs all of the words with the minimum time
     for(int i = 0; i < words.size(); i++) {
         if(times[i] == minimumTime) {
             cout << words[i] << " = " << minimumTime / 4.0 << "s\n";
--- Part2/solution2.cpp
+++ Part2/solution2.cpp
@@ -6,6 +6,7 @@
 
 using namespace std;
 
+// Declares maps for initial state of phone without any broken buttons
 map<int, string> buttonCharacters = {
     {2, "abc"},
     {3, "def"},
@@ -28,6 +29,7 @@ map<char, int> characterButtons = {
     {'w', 9}, {'x', 9}, {'y', 9}, {'z', 9}
 };
 
+// Same function as part 1 that calculates the time to type a word given the button maps
 int wordTime(string word, map<int, string> buttonCharacters, map<char, int> characterButtons) {
     string lowercaseWord = "";
     for(int i = 0; i < word.size(); i++) {
@@ -55,15 +57,19 @@ int wordTime(string word, map<int, string> buttonCharacters, map<char, int> char
     return time - 1;
 }
 
+// Function to change the button maps given a broken button
 void changeMaps(int brokenButton) {
+    // Iterate through the buttons and add the correct number of characters or add none if it is the broken button
     int buttonCount = 1;
     char currentLetter = 'a';
     for(int button = 2; button <= 9; button++) {
+        // If the button is broken
         if(button == brokenButton) {
             buttonCharacters[button] = "";
             continue;
         }
 
+        // Assign either 4 or 3 letters depending on which button
         if(buttonCount == 6 || buttonCount == 7) {
             buttonCharacters[button] = "";
             buttonCharacters[button] += (char)(currentLetter);
@@ -82,6 +88,7 @@ void changeMaps(int brokenButton) {
         buttonCount++;
     }
     
+    // Map each character to their button given the new button to characters map
     for(int i = 2; i <= 9; i++) {
         for(int j = 0; j < buttonCharacters[i].size(); j++) {
             characterButtons[buttonCharacters[i][j]] = i;
@@ -90,8 +97,10 @@ void changeMaps(int brokenButton) {
 }
 
 int main() {
+    // Stores list of words
     vector<string> words;
 
+    // Open file
     ifstream indata;
     indata.open("Part2.txt");
     if(!indata) {
@@ -99,11 +108,14 @@ int main() {
         exit(1);
     }
 
+    // Input the broken button
     int brokenButton;
     indata >> brokenButton;
 
+    // Update the maps given the broken button
     changeMaps(brokenButton);
 
+    // Input the list of words
     string input;
     indata >> input;
     words.push_back(input);
@@ -113,16 +125,19 @@ int main() {
     }
     indata.close();
 
+    // Create the list of times
     vector<int> times;
     int minimumTime = 1000000;
     for(int i = 0; i < words.size(); i++) {
         int currentTime = wordTime(words[i], buttonCharacters, characterButtons);
         times.push_back(currentTime);
+        // Update the minimum
         if(currentTime < minimumTime) {
             minimumTime = currentTime;
         }
     }
 
+    // Output the minimum words
     for(int i = 0; i < words.size(); i++) {
         if(times[i] == minimumTime) {
             cout << words[i] << " = " << minimumTime / 4.0 << "s\n";
